d4eee0e2fc0c5ea12baaffd2204074511a91565d
Jemuel <f2pskilleruel@gmail.com>
2022-04-04 19:07:21 +0300

    Finish the javascript
    Completed the javascript

--- Book/script.js
+++ Book/script.js
@@ -4,6 +4,7 @@
 const library = document.querySelector(".theLibrary")
 const addButton = document.getElementById('AddNewBook')
 const formDisplay = document.getElementById("overlay")
+const submitButton = document.getElementById("theSubmitButton")
 
 var row, cell, text, deleteButton
 
@@ -13,7 +14,7 @@ let myLibrary = [
         title: "The Hobbit",
         author: "J.R.R Tolkien",
         pages: "297",
-        read: "not yet"
+        read: "true"
     }
 ]
 
@@ -24,42 +25,96 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-//need to iterate over inTable
-function displayBook(array,) {
-    array.forEach(element => {
+function addBookToLibrary(array) {
+    for (i = 0; i < array.length; i++) {
         row = document.createElement("tr")
-        for (prop in element) {
-            cell = document.createElement("td")
-            text = document.createTextNode(`${element[prop]}`)
-            cell.appendChild(text)
-            row.appendChild(cell)
-            
+        //Title of the book
+        cell = document.createElement("td")
+        text = document.createTextNode(array[i].title)
+        cell.appendChild(text)
+        row.appendChild(cell)
+        //Author of the book
+        cell = document.createElement("td")
+        text = document.createTextNode(array[i].author)
+        cell.appendChild(text)
+        row.appendChild(cell)
+        //Pages of the book
+        cell = document.createElement("td")
+        text = document.createTextNode(array[i].pages)
+        cell.appendChild(text)
+        row.appendChild(cell)
+        //Read or not?
+        cell = document.createElement("td")
+        var readCheckbox = document.createElement("div")
+        readCheckbox.setAttribute("id", "readValue")
+        if (array[i].read === "true" || array[i].read === true) {
+            text = document.createTextNode("Read")
+            readCheckbox.setAttribute("class", "isRead")
         }
+        else if (array[i].read === "false" || array[i].read === true) {
+            text = document.createTextNode("Unread")
+        }
+        readCheckbox.appendChild(text)
+        cell.appendChild(readCheckbox)
+        row.appendChild(cell)
+        //Delete button
         deleteButton = document.createElement("input")
         deleteButton.setAttribute("type", "button")
         deleteButton.setAttribute("onclick", "deleteRow(this)")
         row.appendChild(deleteButton)
         library.appendChild(row)
         myLibrary = []
-    })
+    }  
 }
 
 function deleteRow(r) {
-    var i = r.parentNode.rowIndex;
-    document.getElementById("myTable").deleteRow(i);
+    let areYouSure = confirm("Are you sure you want to delete this book?")
+    if (areYouSure === true) {
+        var i = r.parentNode.rowIndex;
+        document.getElementById("myTable").deleteRow(i)
+    }
+    else {
+        return
+    }
 }
 
 addButton.addEventListener("click", () => {
     formDisplay.style.display = "block";
 })
 
-displayBook(myLibrary)
+addBookToLibrary(myLibrary)
 
-//form data
-document.querySelector("form").addEventListener("submit", (e) => {
-    const data = Object.fromEntries(new FormData(e.target).entries())
-    myLibrary.push(data)
+//grabbing form data
+submitButton.addEventListener("click", () => {
+    var title = document.getElementById("bookTitle").value
+    var author = document.getElementById("bookAuthor").value
+    var pages = document.getElementById("bookPages").value
+    var read = document.getElementById("bookRead").checked
+    read = read.toString()
+    var ABook = new Book(title, author, pages, read)
+    myLibrary.push(ABook)
+    console.log(myLibrary)
     document.getElementById("bookDetails").reset()
     formDisplay.style.display = "none"
-    displayBook(myLibrary)
+    addBookToLibrary(myLibrary)
+})
+
+//read or unread toggle
+function readToggling() {
+    const readToggle = document.querySelector("div#readValue")
+    readToggle.addEventListener("click", (e) => {
+        e.target.removeAttribute("class")
+    })
+}
+
+document.addEventListener("click", (e) => {
+   if (e.target.classList.contains("isRead") === true) {
+       e.target.removeAttribute("class")
+       e.target.textContent = "Unread"
+
+   }
+   else if (e.target.textContent === "Unread") {
+        e.target.setAttribute("class", "isRead")
+        e.target.textContent = "Read"
+   }
 })
